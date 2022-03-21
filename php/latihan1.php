a2ad5b462d6547ae5a60dc646cc5670ab36b20cd
Gangstahhh <riskymuhamad352@gmail.com>
2022-03-21 20:06:04 +0700

    Latihan ke 1

---
+++ php/latihan1.php
@@ -0,0 +1,107 @@
+
+<?php 
+/*  Di bagian Ini membahas Tentang Komentar
+// ini adalah Komentar
+// echo "Hello Wordl!";
+*/
+
+
+/*PERTEMUAN KE 1 TENTANG SYNTAX PHP 
+    echo, print
+    print_r = Untuk print Array
+    var_dump = untuk print informasi tentang Variabel
+*/
+
+//SILAHKAN PAKAI KOMEN UNTUK MENDISABLE KODE PROGRAM UNTUK MELIHAT HASILLNYA SATU PERSATU
+// Note: Untuk Angka Tidak Harus Memakai petik
+
+echo "Muhammad Risky";
+
+// Print "Muhammad Risky";
+
+// print_r("Muhammad Risky");
+
+// var_dump("Muhammad Risky");
+
+// NGEPRINT BOOLEAN 
+// echo true; // Value nya akan 1
+
+// echo false; // Tidak Tampi Apa-Apa
+
+
+/* PENULISAN SYNTAX PHP */
+// 1. Php di dalam Html
+// 2. html di dalam php
+
+// VARIABEL
+// $nama = "Muhammad Risky";
+
+// OPERATOR
+
+//  Aritmatrika(+ - * / % )
+// $a = 4;
+// $b = 5;
+// echo $a + $b 
+
+
+// CONCAT / Pengabung String ( . )
+// $nama_depan = "Muhammad";
+// $nama_belakang = "Risky";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment (= += -= /= %= .=)
+// $x = 1;
+// $x -= 5;
+// echo $x;
+
+
+// Perbandingan (> < >= <= ==)
+// echo 4==1;
+// echo 5 > 4;
+
+
+// Identitas (=== !==)
+// var_dump(1=== "1");    
+
+// Logika (&& || !)
+
+//$x = 10;
+//var_dump(20>$x || 20<$x);
+
+
+
+?>
+
+<!-- php di dalam html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1> Selamat Datang <?php //echo $nama ?></h1>
+</body>
+</html>
+-->
+
+<!-- html di dalam php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php 
+        
+        // echo "Halo Muhammad Risky";
+
+    ?>
+</body>
+</html>
+-->
